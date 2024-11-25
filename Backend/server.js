61e982ac80fc4862f8a741dc148c557d00701f47
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-11-25 09:00:05 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -15,31 +15,36 @@ dotenv.config({
 const port = process.env.PORT || 5000;
 const app = express();
 
-// Middleware
-app.use(morgan('dev')); // Request logging
-app.use(cors()); 
-app.use(helmet()); // Add security headers
-app.use(bodyParser.json()); // Parse JSON request bodies
-app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+app.use(cors({
+    origin: ['https://job-listnig-j4l1.vercel.app'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+})); 
+app.use(morgan('dev')); 
+app.use(helmet()); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(function(err, req, res, next) {
-    console.error(err); // Log the error for debugging
-    res.status(500).json({ message: err.message }); // Send error message to the client
+    console.error(err); 
+    res.status(500).json({ message: err.message }); 
   });
 
-// Sample route
+
 app.get('/api/v1', (req, res) => {
     console.log("Welcome to the backend");
     res.send("Welcome to the backend");
 });
 
-// main routes
+
 app.use('/api/v1/jobs',Jobs)
 app.use('/api/v1/auth',Auth)
 
-// Database connection
+
 dbconnect();
 
-// Start the server
+
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
 });
